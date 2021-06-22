af2934945e1d3f7384a9b1455d8b1726af611704
dasf1 <tm0534917328@gmail.com>
2021-06-22 18:47:07 -0400

    add display method

--- java_class.java
+++ java_class.java
@@ -1,9 +1,16 @@
 public class java_class {
 
-//this is print statment 
+//this is print statment
+public static void display(){
+
+System.out.println("output from method ");
+
+} 
 	public static void main(String[]args){
 	
 		System.out.println("project test");
+                display();
 	}
 
 }
+

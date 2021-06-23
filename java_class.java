fd6a7edb857e40766573d8a6e70cfe5aa390f010
dasf1 <tm0534917328@gmail.com>
2021-06-22 21:03:39 -0400

    some changes in style of code for step 7

--- java_class.java
+++ java_class.java
@@ -1,23 +1,29 @@
 public class java_class {
 
-//this is print statment
+//this is print method
+
 public static void display(){
 
 System.out.println("output from method ");
 
-} 
-	public static void main(String[]args){
-	
-		System.out.println("project test");
-                display();
+}
+
+public static void main(String[]args){
+
+
+	System.out.println("project test");
+
+        display();
 
 
 //this for loop added in step6 .
         for(int x=0;x<10;x++){
 
       System.out.println(x);
+
+
 }
 	}
 
-}
+             }
 
